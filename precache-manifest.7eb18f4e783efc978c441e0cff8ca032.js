fd522a5e9cf230efdaa4d73c462fd7b4a6b7470f
sonhoai27@gmail.com <sonhoai27@gmail.com>
2019-06-25 02:40:23 +0700

    add new site

---
+++ precache-manifest.7eb18f4e783efc978c441e0cff8ca032.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20382cf29adc680bc0aa5d16eb3726c",
+    "url": "/index.html"
+  },
+  {
+    "url": "/js/1.5fb56bfb893fb35c75e5.js"
+  },
+  {
+    "url": "/js/main.5fb56bfb893fb35c75e5.js"
+  },
+  {
+    "url": "/manifest.13a7f46c0959f20382785266f8730ec2.json"
+  }
+]);
